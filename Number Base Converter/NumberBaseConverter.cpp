77ab6711b783cb57ed63b9697e06b66aa165c555
TrussedBoat <85097044+TrussedBoat@users.noreply.github.com>
2021-05-31 19:34:24 +0300

    Add files via upload

---
+++ Number Base Converter/NumberBaseConverter.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+string DecimalToHexadecimal(int decimalNumber) {
+	//Written By TrussedBoat
+	char hexaNumbers[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+	string hexadecimalNumber;
+
+	unsigned int current = 1;
+	while (current * 16 <= decimalNumber) {
+		current *= 16;
+	}
+
+	int tempNumber = decimalNumber;
+	while (tempNumber >= 0 && current >= 1) {
+		hexadecimalNumber += hexaNumbers[tempNumber / current];
+		tempNumber %= current;
+		current /= 16;
+	}
+
+	return hexadecimalNumber;
+}
+
+int main() {
+	cout << "Welcome to \"Base Converter\"" << endl;
+	cout << "Write the number you want to turn to hexadecimal" << endl;
+	cout << "When you need to quit, just type \"-1\"" << endl << endl;
+
+	int a = 0;
+	while (true) {
+		cout << "Decimal = ";
+		cin >> a;
+		if (a == -1) {
+			break;
+		}
+		cout << "Hexadecimal = " << DecimalToHexadecimal(a) << endl << endl;
+	}
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+Base Converter - 1.0
+
+
+Updates:
+-You can now convert decimal numbers to hexadecimal numbers.
+
+
+Description:
+This program lets you convert the bases of the numbers.
+
+~To use the program:
+	-Run "NumberBaseConverter.exe" in "Debug" folder
+	-Enter the number you want to convert to hexadecimal
+
+~To look at the source code:
+	-Open the "NumberBaseConverter.cpp" in "NumberBaseConverter" folder
+
+
+Thanks and have fun.
